b4f5016497cde227837a9b0553ba2efc206bdbdb
D3Portillo <d3portillo@gmail.com>
2025-12-02 08:44:35 +0100

    chores: ui mock

---
+++ app/components/icons/index.tsx
@@ -0,0 +1,19 @@
+export const Logo = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 100 100"
+  >
+    <path
+      fill="currentColor"
+      d="M78.6 16.9c2.5 0 4.5 2 4.5 4.5v57.2c0 2.5-2 4.5-4.5 4.5H21.4c-2.5 0-4.5-2-4.5-4.5V21.4c0-2.5 2-4.5 4.5-4.5z"
+    />
+    <path
+      fill="currentColor"
+      d="M85.3 0C93.4 0 100 6.6 100 14.7v70.6c0 8.1-6.6 14.7-14.7 14.7H14.7C6.6 100 0 93.4 0 85.3V14.7C0 6.6 6.6 0 14.7 0zM14.7 8.5a6 6 0 0 0-6.2 6.2v70.6c0 3.4 2.7 6.2 6.2 6.2h70.6c3.4 0 6.2-2.8 6.2-6.2V14.7c0-3.4-2.8-6.2-6.2-6.2z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+    />
+  </svg>
+)
